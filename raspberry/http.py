0b8e18f4ea813b7f0d0b6d7cd1a63e90e2bd7072
asminex <a.s.minimulin@gmail.com>
2018-01-30 17:14:24 +0300

    Delete the trash and make structure of project

--- raspberry/http.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def classify(file_name):
-    url = 'http://192.168.0.133:5000/classify/'
-    file = open(file_name,'rb')
-    r = requests.post(url,files={'image':file}, data={'file_name':file_name})
-    return r.text
