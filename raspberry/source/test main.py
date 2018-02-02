0f631e468afb4e7765fec42490c006e3b0a8b9d1
Egor Spirin <qwertyxxxwww@gmail.com>
2018-02-02 15:12:48 +0300

    Update test main.py

--- raspberry/source/test main.py
+++ raspberry/source/test main.py
@@ -1,11 +1,13 @@
 from config import *
 import RPi.GPIO as GPIO
 import time
-import urllib4
 import picamera
 import logging
 import datetime
 import send
+import requests
+import os
+from multiprocessing import Process
 
 import Adafruit_PCA9685
 import serial
@@ -13,11 +15,8 @@ import serial
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(BEEPER, GPIO.OUT)
-GPIO.output(BEEPER, 1)
+GPIO.output(BEEPER, 0)
 
-CurGreen = 0
-CurRed = 0
-CurBlue = 0
 
 camera = picamera.PiCamera()
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)  # Setting up logging
@@ -48,20 +47,11 @@ GPIO.setup(TRIG3, GPIO.OUT)
 GPIO.setup(ECHO3, GPIO.IN)
 
 WAIT_LIMIT = 8
+command = ''
 
 
-def check_rfid():
-    code = ''
-    while True:
-        #logging.info(UART.read().decode("utf-8"))
-        if UART.read() == b'\x02':
-            code = UART.read(12)
-            UART.flushInput()
-
-            break
-    code = code.decode("utf-8")
-    logging.info(code)
-    return code
+def fun():
+    os.system(command)
 
 
 def open_up():
@@ -95,7 +85,6 @@ def open_lock():
 
 
 def make_photo():
-    GPIO.output(INNER, 1)
     image_time = datetime.datetime.now()  # Making a unique name of a string from datetime
     image_time = str(image_time)[:19].replace(' ', '').replace('-', '').replace(':', '')
     image_path = '/home/pi/Documents/Photos/' + image_time + '.jpeg'
@@ -111,24 +100,108 @@ def contents_type():
     image = make_photo()
     return send.get_class(image)
 
-def waiting():
-    global CurGreen, CurBlue
-    for i in range(1000):
-        pwm.set_pwm(GREEN, 0, CurGreen)
-        pwm.set_pwm(BLUE, 0,  CurBlue)
-        CurGreen -= 2
-        CurBlue += 2
-        time.sleep(0.001)
 
-    for i in range(1000):
+def waiting():  # Dynamic color change while waiting
+    global CurGreen, CurBlue, CurRed
+    CurGreen = 4000
+    CurRed = 0
+    CurBlue = 2000
+
+    try:
+        pwm.set_pwm(RED, 0, CurRed)
         pwm.set_pwm(GREEN, 0, CurGreen)
         pwm.set_pwm(BLUE, 0, CurBlue)
-        CurGreen += 2
-        CurBlue -= 2
-        time.sleep(0.001)
+    except:
+        time.sleep(0.1)
+        pwm.set_pwm(RED, 0, CurRed)
+        pwm.set_pwm(GREEN, 0, CurGreen)
+        pwm.set_pwm(BLUE, 0, CurBlue)
+
+    while True:
+        for i in range(250):
+            try:
+                pwm.set_pwm(GREEN, 0, CurGreen)
+                pwm.set_pwm(BLUE, 0,  CurBlue)
+            except:
+                time.sleep(0.1)
+                pwm.set_pwm(GREEN, 0, CurGreen)
+                pwm.set_pwm(BLUE, 0, CurBlue)
+            CurGreen -= 8
+            CurBlue += 8
+            if i % 30 == 0 and UART.read() == b'\x02':
+                return
+
+        for i in range(250):
+            try:
+                pwm.set_pwm(GREEN, 0, CurGreen)
+                pwm.set_pwm(BLUE, 0, CurBlue)
+            except:
+                time.sleep(0.1)
+                pwm.set_pwm(GREEN, 0, CurGreen)
+                pwm.set_pwm(BLUE, 0, CurBlue)
+            CurGreen += 8
+            CurBlue -= 8
+            if i % 30 == 0 and UART.read() == b'\x02':
+                return
 
 
-def sonic_check(TRIG,ECHO):
+def static_color(port):  # Just displaying a color
+    global CurGreen, CurBlue, CurRed	
+    port -= 8
+
+    colors = [CurRed, CurGreen, CurBlue]
+    while colors[port] != 4000 or sum(colors) != 4000:
+        for i in range(3):
+            if i == port:
+                colors[i] = min(4000, colors[i] + 25)
+            else:
+                colors[i] = max(0, colors[i] - 25)
+            try:
+                pwm.set_pwm(i + 8, 0, colors[i])
+            except:
+                time.sleep(0.1)
+                pwm.set_pwm(i + 8, 0, colors[i])
+
+    CurRed = colors[0]
+    CurGreen = colors[1]
+    CurBlue = colors[2]
+
+
+def dynamic_color(port):  # Basically blink
+    global CurGreen, CurBlue, CurRed
+    port -= 8
+
+    colors = [0, 0, 0]
+    colors[port] = 4000
+    for i in range(3):
+        try:
+            pwm.set_pwm(i + 8, 0, colors[i])
+        except:
+            time.sleep(0.1)
+            pwm.set_pwm(i + 8, 0, colors[i])
+
+    while colors[port] > 0:
+        colors[port] = max(0, colors[port] - 25)
+        try:
+            pwm.set_pwm(port + 8, 0, colors[port])
+        except:
+            time.sleep(0.1)
+            pwm.set_pwm(port + 8, 0, colors[port])
+
+    while colors[port] < 4000:
+        colors[port] = min(4000, colors[port] + 25)
+        try:
+            pwm.set_pwm(port + 8, 0, colors[port])
+        except:
+            time.sleep(0.1)
+            pwm.set_pwm(port + 8, 0, colors[port])
+
+    CurRed = colors[0]
+    CurGreen = colors[1]
+    CurBlue = colors[2]
+
+
+def sonic_check(TRIG, ECHO):
     GPIO.output(TRIG, False)
     time.sleep(0.01)
     GPIO.output(TRIG, True)
@@ -145,7 +218,7 @@ def sonic_check(TRIG,ECHO):
     distance = pulse_duration * 17150
     distance = round(distance, 2) - 0.5
 
-    if (distance > 42) or (distance < 37):
+    if (distance > 45) or (distance < 35):
         return True
     return False
 
@@ -153,10 +226,10 @@ def sonic_check(TRIG,ECHO):
 def something_in():
     k = 0
     for i in range(30):
-        if sonic_check(TRIG1,ECHO1):
+        if sonic_check(TRIG1, ECHO1):
             k += 1
         time.sleep(0.0001)
-        if sonic_check(TRIG2,ECHO2):
+        if sonic_check(TRIG2, ECHO2):
             k += 1
     if k < 10:
         return False
@@ -165,10 +238,11 @@ def something_in():
 
 def cheat_check():
     k = 0
-    for i in range(30):
-        if sonic_check(TRIG3,ECHO3):
+    for i in range(50):
+        if sonic_check(TRIG3, ECHO3):
             k += 1
-    if k < 10:
+            time.sleep(0.01)
+    if k >= 1:
         return False
     return True
 
@@ -176,9 +250,9 @@ def cheat_check():
 def user_reg(user_id):
     user_url = 'http://smartbin35.ru.mastertest.ru/api/checkuser?cardCode=' + user_id
     logging.info(user_id)
-    response = urllib.request.urlopen(user_url)
-    logging.info(response.read())
-    user_status = response.read()
+    response = requests.get(user_url)
+    logging.info(response.text)
+    user_status = response.text
     if user_status == b'true':
         return True
     return False
@@ -187,51 +261,59 @@ def user_reg(user_id):
 def reward(user_id, trash_type):
     reward_url = 'http://smartbin35.ru.mastertest.ru/api/bonus?cardCode=' + user_id
     reward_url = reward_url + '&trashType=' + trash_type + '&trashId=2'
-    urllib.request.urlopen(reward_url)
+    response = requests.get(reward_url)
+    return response
 
 
 def beep():
-    GPIO.output(BEEPER, 0)
-    time.sleep(0.3)
     GPIO.output(BEEPER, 1)
+    time.sleep(0.3)
+    GPIO.output(BEEPER, 0)
 
 try:
     time.sleep(1)
     close_lock()
     logging.info("Process started")
+    close_up()
+    close_down()
+    close_lock()
     while True:
-        # Correct
-        close_up()
-        close_down()
-
-        user = check_rfid()
-
-        if not(user):
-            continue
+        waiting()
+        user = UART.read(12).decode('utf-8')
+        logging.info(user)
 
         beep()
 
-        if user == "5605B8DF7642":
+        if user == "5605B8DF764":  # Maintenance stuff
             open_lock()
+            user = ""
+            static_color(RED)
             logging.info("The bin is opened for maintenance")
-            reward(user, 'pet')
+            time.sleep(2)
+            UART.flushInput()
             while True:
-                time.sleep(5)
-        reg_stat = 0
+                time.sleep(0.2)
+                if UART.read() == b'\x02':
+                    userok = UART.read(12).decode('utf-8')
+                    if userok == "5605B8DF7642":
+                        beep()
+                        logging.info("The bin is normal mode")
+                        break
         try:
             reg_stat = user_reg(user)
         except:
+            static_color(RED)
             time.sleep(1)
-            UART.flushInput()
         if reg_stat:
+            static_color(GREEN)
             logging.info("User found")
-            # Correct
             entry_time = time.time()
             time_exit = False
 
             while not time_exit:
                 find_exit = False
                 open_up()
+                GPIO.output(INNER, 1)
                 while (not find_exit) and (not time_exit):
                     time.sleep(0.1)
                     current_time = time.time()
@@ -239,39 +321,45 @@ try:
                         time_exit = True
                     if something_in():
                         find_exit = True
+
                 time.sleep(0.5)
+                while not(cheat_check()):
+                    beep()
                 close_up()
                 time.sleep(0.5)
                 if not find_exit:
                     continue
 
                 if something_in():
-                    time.sleep(0.7)
                     logging.info("Something in")
                     inner_type = ""
-                    if fake == 1:
-                        fake = 0
-                    else:
-                        fake = 1
                     try:
                         inner_type = contents_type()
-
                     except:
-                        time.sleep(5)
-                        time_exit = True
+                        static_color(RED)
+                        while True:
+                            time.sleep(1)
 
-                    entry_time = time.time()
-                    find_exit = False
                     if inner_type == 'pet':
+                        command = 'mplayer /home/pi/pywork/sounds/plastic.mp3 -af volume=7'
+                        p = Process(target=fun)
+                        p.start()
                         pet_down()
                         time.sleep(2.5)
                         close_down()
+                        p.terminate()
                         try:
                             reward(user, 'pet')
                         except:
                             time.sleep(0.5)
                         logging.info('Rewarded for pet!')
+                        for i in range(2):
+                            dynamic_color(GREEN)
+
                     elif inner_type == 'al':
+                        p = Process(target=fun)
+                        command = 'mplayer /home/pi/pywork/sounds/al.mp3 -af volume=7'
+                        p.start()
                         al_down()
                         time.sleep(2.5)
                         close_down()
@@ -280,27 +368,37 @@ try:
                         except:
                             time.sleep(0.5)
                         logging.info('Rewarded for al!')
-                    else:
-                        # Correct
+                        for i in range(2):
+                            dynamic_color(GREEN)
 
+                    else:
                         open_up()
+                        p = Process(target=fun)
+                        command = 'mplayer /home/pi/pywork/sounds/unknown.mp3 -af volume=7'
+                        p.start()
                         while something_in():
-                            time.sleep(0.01)
-                        time.sleep(1)
+                            for i in range(2):
+                                dynamic_color(RED)
+                        time.sleep(0.5)
+                        while not (cheat_check()):
+                            beep()
                         close_up()
+                        p.terminate()
                         time_exit = True
+
+                    entry_time = time.time()
+                    find_exit = False
                 else:
                     entry_time = time.time()
-                    # Correct
+                    dynamic_color(RED)
+
             logging.info("Session closed")
             logging.info('****************************')
         else:
-            pass
-            # Correct
+            dynamic_color(RED)
 
         time.sleep(0.5)
         UART.flushInput()
-
 
 finally:
     GPIO.cleanup()
