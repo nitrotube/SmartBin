b05db9d2598745a1a0e296c1b70967bb766e55be
Egor Spirin <qwertyxxxwww@gmail.com>
2018-02-11 21:58:26 +0300

    Email settings update

--- raspberry/source/warn_full.py
+++ raspberry/source/warn_full.py
@@ -1,17 +1,17 @@
 import smtplib
 from email.mime.text import MIMEText
 
-def send_warn_full():
-    server_addr  = 'smtp.gmail.com'
-    port = 587
-    pi_login = 'smartbin.warner'
-    pi_pass = 'sbwarner2018'
+def send_warn_full(type):
+    server_addr  = 'smtp.timeweb.ru'
+    port = 25
+    pi_login = 'warning@smartbin.ru'
+    pi_pass = 'warning12'
     to = 'e.spirin@smartbin.ru'
 
-    body = '<h1 color = "red" font = "Times New Roman">Musorka zabita</h1>'
+    body = '<h1 color = "red" font = "Times New Roman">The bin is full of ' + type + ' </h1>'
     title = 'SBWarning'
     msg = MIMEText((body),'html')
-    
+
     msg['From'] = 'SBWarner'
     msg['To'] = 'Somebody'
     msg['Subject'] = title
@@ -22,6 +22,3 @@ def send_warn_full():
     server.login(pi_login, pi_pass)
     server.sendmail(pi_login, to, message)
     server.quit()
-
-if __name__=='__main__':
-    send_warn_full()
